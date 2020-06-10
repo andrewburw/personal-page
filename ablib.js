67aac0585c417a0d62a715dc97331e5ed064fa67
andrewburw <30740560+andrewburw@users.noreply.github.com>
2020-06-10 17:18:07 +0300

    Added galery

---
+++ ablib.js
@@ -0,0 +1,59 @@
+var $ = (function () {
+
+	//'use strict';
+
+	let Constructor = function (selector) {
+		this.elems = document.querySelectorAll(selector);
+	};
+
+
+/*
+	Constructor.prototype.attr = function (name,value) {
+
+   if (value == null) {
+
+			return this.elems[0].getAttribute(name) ;
+   } else {
+
+	  		this.elems[0].setAttribute(name, value)
+   }
+
+
+
+    };
+
+		Constructor.prototype.htmL = function (ht) {
+					this.elems[0].innerHTML = ht;
+			};
+		Constructor.prototype.hide = function () {
+        this.elems[0].classList.remove('visible');
+        this.elems[0].classList.add('hidden');
+
+				};
+
+		Constructor.prototype.show = function () {
+
+						this.elems[0].classList.remove('hidden');
+	          this.elems[0].classList.add('visible');
+
+
+	  				};
+ */
+
+ Constructor.prototype.onClick = function (callBack) {
+
+     this.elems[0].addEventListener("click", callBack);
+	 };
+
+Constructor.prototype.hide = function () {
+ 			this.elems[0].classList.remove('visible');
+ 			this.elems[0].classList.add('hidden');
+
+ 			};
+  let instantiate = function (selector) {
+  		return new Constructor(selector);
+  	};
+
+	return instantiate;
+
+})();
